6f389ccccfba675ae9da05283ddab1094968e22f
Steve Garward <garwards@gmail.com>
2015-11-15 15:14:50 -0600

    Updated for changes to Subsystems

--- src/org/wildstang/yearly/subsystems/WSSubsystems.java
+++ src/org/wildstang/yearly/subsystems/WSSubsystems.java
@@ -1,22 +1,34 @@
 package org.wildstang.yearly.subsystems;
 
-public enum WSSubsystems
+import org.wildstang.framework.core.Subsystems;
+
+
+public enum WSSubsystems implements Subsystems
 {
-   DRIVE_BASE("Drive base"),
-   HARDWARE_TEST("Hardware test"),
-   LED("LED"),
-   MONITOR("Monitor");
+   DRIVE_BASE("Drive base", DriveBase.class),
+   HARDWARE_TEST("Hardware test", HardwareTest.class),
+   LED("LED", LED.class),
+   MONITOR("Monitor", Monitor.class);
 
    private String m_name;
+   private Class m_class;
 
-   WSSubsystems(String p_name)
+   WSSubsystems(String p_name, Class p_class)
    {
       m_name = p_name;
+      m_class = p_class;
    }
 
+   @Override
    public String getName()
    {
       return m_name;
+   }
+
+   @Override
+   public Class getSubsystemClass()
+   {
+      return m_class;
    }
 
 }
