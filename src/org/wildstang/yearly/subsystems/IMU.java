c0ff8f6b3bb37bc1043d4c9c04bf09bf94d5deec
wdoherty <billydoherty5104@gmail.com>
2015-12-08 14:36:18 -0600

    progress

--- src/org/wildstang/yearly/subsystems/IMU.java
+++ src/org/wildstang/yearly/subsystems/IMU.java
@@ -19,9 +19,14 @@ public class IMU implements Subsystem
 
    MessageHandler messageSender;
    // Sent states
-   boolean autoDataSent = false;
-   boolean disableDataSent = false;
-   boolean sendData = false;
+   boolean dataReceived;
+   static byte[] IMUData = new byte[6];
+//   PosX1
+//   PosX2
+//   PosY1
+//   PosY2
+//   Heading1
+//   Heading2
 
    private String m_name;
 
@@ -46,26 +51,6 @@ public class IMU implements Subsystem
       }
    }
 
-   /*
-    * | Function | Cmd | PL 1 | PL 2 |
-    * --------------------------------------------------------- | Shoot | 0x05 |
-    * 0x13 | 0x14 | | Climb | 0x06 | 0x11 | 0x12 | | Autonomous | 0x02 | 0x11 |
-    * 0x12 | | Red Alliance | 0x04 | 0x52 | 0x01 | | Blue Alliance | 0x04 | 0x47
-    * | 0x01 |
-    *
-    * Send sequence once, no spamming the Arduino.
-    */
-
-   // Reused commands from year to year
-   LedCmd autoCmd = new LedCmd(0x02, 0x11, 0x12);
-   LedCmd redCmd = new LedCmd(0x04, 0x52, 0x01);
-   LedCmd blueCmd = new LedCmd(0x04, 0x47, 0x01);
-
-   // New commands each year
-   LedCmd shootCmd = new LedCmd(0x05, 0x13, 0x14);
-   LedCmd climbCmd = new LedCmd(0x06, 0x11, 0x12);
-   LedCmd intakeCmd = new LedCmd(0x07, 0x11, 0x12);
-
    public IMU(String name)
    {
       m_name = name;
@@ -75,102 +60,22 @@ public class IMU implements Subsystem
       // the subsystem container.
       t.start();
 
-      // Kicker
-//      Core.getInputManager().getInput(WSInputs.MAN_BUTTON_6.getName()).addInputListener(this);
-      // Intake
-//      Core.getInputManager().getInput(WSInputs.MAN_BUTTON_5.getName()).addInputListener(this);
-      // Climb
-//      Core.getInputManager().getInput(WSInputs.DRV_BUTTON_2.getName()).addInputListener(this);
    }
 
    @Override
    public void init()
    {
       // Nothing to do anymore, I'm bored.
-      autoDataSent = false;
-      disableDataSent = false;
-      sendData = false;
+      
+      dataReceived = false;
    }
 
    @Override
    public void update()
    {
-      // Get all inputs relevant to the LEDs
-      boolean isRobotEnabled = DriverStation.getInstance().isEnabled();
-      boolean isRobotTeleop = DriverStation.getInstance().isOperatorControl();
-      boolean isRobotAuton = DriverStation.getInstance().isAutonomous();
-      DriverStation.Alliance alliance = DriverStation.getInstance().getAlliance();
 
-      if (isRobotEnabled)
-      {
-         if (isRobotTeleop)
-         {
-            // Do nothing, handled in acceptNotification
-         }
-         else if (isRobotAuton)
-         {
-            // --------------------------------------------------------------
-            // Handle Autonomous signalling here
-            // --------------------------------------------------------------
-            // One send and one send only.
-            // Don't take time in auto sending LED cmds.
-            if (!autoDataSent)
-            {
-               autoDataSent = sendData(autoCmd);
-            }
-         }
       }
-      else
-      {
-         // ------------------------------------------------------------------
-         // Handle Disabled signalling here
-         // ------------------------------------------------------------------
-         switch (alliance)
-         {
-            case Red:
-            {
-               if (!disableDataSent)
-               {
-                  disableDataSent = sendData(redCmd);
-               }
-            }
-            break;
 
-            case Blue:
-            {
-               if (!disableDataSent)
-               {
-                  disableDataSent = sendData(blueCmd);
-               }
-            }
-            break;
-            default:
-            {
-               disableDataSent = false;
-            }
-            break;
-         }
-      }
-   }
-
-   @Override
-   public void inputUpdate(Input source)
-   {
-//      if (((DigitalInput) source).getName().equals(WSInputs.MAN_BUTTON_6.getName()))
-//      {
-//         if (((DigitalInput) source).getValue())
-//         {
-//            sendData(shootCmd);
-//         }
-//      }
-//      else if (((DigitalInput) source).getName().equals(WSInputs.DRV_BUTTON_2.getName()))
-//      {
-//         if (((DigitalInput) source).getValue())
-//         {
-//            sendData(climbCmd);
-//         }
-//      }
-   }
 
    @Override
    public void selfTest()
@@ -227,21 +132,7 @@ public class IMU implements Subsystem
                   this.wait();
                   // Need at least 5 bytes and someone has to have called
                   // setSendData.
-//                  if (sendSize >= 5 && dataToSend)
-//                  {
-//                     // Extremely fast and cheap data confirmation
-//                     // algorithm
-//                     sendData[3] = (byte) (~sendData[1]);
-//                     sendData[4] = (byte) (~sendData[2]);
-//                     // byte[] dataToSend, int sendSize, byte[]
-//                     // dataReceived, int receiveSize
-//                     // set receive size to 0 to avoid sending an i2c
-//                     // read request.
-//                     // System.out.println("Cmd: " +
-//                     // Integer.toHexString(sendData[0]));
-//                     i2c.transaction(sendData, sendSize, rcvBytes, 0);
-//                     dataToSend = false;
-//                  }
+                  i2c.transaction(sendData, 0, IMUData, IMUData.length);
                }
                catch (InterruptedException e)
                {
@@ -249,17 +140,23 @@ public class IMU implements Subsystem
             }
          }
       }
-
+      
       public void setSendData(byte[] data, int size)
       {
          sendData = data;
          sendSize = size;
          dataToSend = true;
       }
-
+      
       public void stop()
       {
          running = false;
       }
    }
+
+@Override
+public void inputUpdate(Input source) {
+	// TODO Auto-generated method stub
+	
+}
 }
