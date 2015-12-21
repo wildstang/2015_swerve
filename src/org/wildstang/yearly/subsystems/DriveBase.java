c448cd5cf041e33292e9836a8dd0af46aef4e30c
Steve Garward <garwards@gmail.com>
2015-12-21 16:05:46 -0600

    .

--- src/org/wildstang/yearly/subsystems/DriveBase.java
+++ src/org/wildstang/yearly/subsystems/DriveBase.java
@@ -189,7 +189,7 @@ public class DriveBase implements Subsystem
 
          magnitude = Math.sqrt(Math.pow(leftX, 2) + Math.pow(leftY, 2)); // here we get the raw magnitude from Pythagorean Theorem
 
-         double angleDistance = getAngleDistance(encodeAngleUR, desiredAngleUR);
+         double angleDistance = getAngleDistance(encodeAngleUL, desiredAngleUL);
          Core.getStateTracker().addState("angleDistance", "angleDistance", angleDistance);
 //         if (angleDistance > Math.PI / 2)
 //         {
